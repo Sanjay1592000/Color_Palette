2f640a384222fb030cd6511d27216fe44f6b549c
Sanjay159000 <sanjaymani9870@gmail.com>
2023-10-10 13:30:47 +0530

    random function added

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ let red = document.getElementById("red");
 let blue = document.getElementById("blue");
 let green = document.getElementById("green");
 
+function randomval(){
+    let min = 0;
+    let max = 255;
+    let randomInt;
+    randomInt= Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomInt;
+}
+
+function randomcolor()
+{
+    let bgcolor = document.getElementById("color");
+    let colval = document.getElementById("colorval1");
+    let colval1 = document.getElementById("colorval2");
+    let r = randomval();
+    let b = randomval();
+    let g = randomval();
+    let x = hexcon(r);
+    let y = hexcon(b);
+    let z = hexcon(g);
+    colval1.innerHTML = `#${x}${y}${z}`;
+    bgcolor.style.backgroundColor = `rgb(${r},${b},${g})`;
+    document.body.style.backgroundColor = `rgb(${r},${b},${g}, 0.4)`;    
+    colval.innerHTML = `rgb(${r},${b},${g})`;
+}
+
+
 function hexcon(d){
     decimal = parseInt(d,10);
     let hexadecimal = decimal.toString(16).toUpperCase();
